e804c1fd01512c1078df041b9b807caf11b0d487
gsy <gtcxg@hotmail.com>
2013-04-14 21:33:01 +0800

    max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -4,6 +4,7 @@ class heap:
     """heap"""
     def __init__(self, array):
         self.array = array
+        self.size = len(self.array)
 
     def get_parent(self, i):
         if i == 0:
@@ -15,13 +16,13 @@ class heap:
             return self.array[i/2-1]
         
     def get_left_child(self, i):
-        if (2*i+1) >= len(self.array):
+        if (2*i+1) >= self.size:
             return None
         else:        
             return self.array[2*i+1]
 
     def get_right_child(self, i):
-        if (2*i+2) >= len(self.array):
+        if (2*i+2) >= self.size:
             return None
         else:
             return self.array[2*i+2]
@@ -55,28 +56,39 @@ class max_heap(heap):
 
         if (i != max_index):            
             self.exchange(i, max_index)
-        self.show()
-        if i%2 == 1:
-            self.max_heapify(i/2)
-        else:
-            self.max_heapify(i/2-1)
+            self.show()
+            if i%2 == 1:
+                self.max_heapify(i/2)
+            else:
+                self.max_heapify(i/2-1)
 
     def build_max_heap(self):
-        for i in range(0, len(self.array)/2):
+        for i in range(0, self.size/2):
             print i
             self.max_heapify(i)
 
     def heap_sort(self):
         result = []
-        for i in range(0, len(self.array)):
+        for i in range(0, len(self.array)/2):
             self.build_max_heap()
             tmp = self.array.pop(0)
             result.append(tmp)
 
         return result
+
+    def another_heap_sort(self):
+        for i in range(0, self.size):
+            self.build_max_heap()
+            self.exchange(0, (self.size - 1))
+            self.size = self.size - 1
+            print '-'*25
+            self.show()
+            print 
+                        
         
 if __name__ == '__main__':
     h = max_heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
-    h.build_max_heap()
-    result = h.heap_sort()
-    print result
+    # h.build_max_heap()
+    # result = h.heap_sort()
+    # print result
+    h.another_heap_sort()
